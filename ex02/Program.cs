169e239871013296c9d5d378b42455e50d4e0d4f
EDUARDO ALCANTARA DE SOUZA <eduardo.asouza23@senacsp.edu.br>
2025-01-13 19:55:30 -0300

    Primeiro Projeto

---
+++ ex02/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ex.02 - Escreva um programa que pergunte o mome do usuário, armazene esse nome numa variável name e apresente uma menssagem de boas-vindas exibindo o nome do usuário
+
+Console.WriteLine("Informe o seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a) " + name);
